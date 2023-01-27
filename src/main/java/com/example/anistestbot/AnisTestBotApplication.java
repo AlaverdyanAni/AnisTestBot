f8ca1e43808d9b1b9a0325cf96181bbfdc5ee545
Ani <ani-alaverdyan@mail.ru>
2023-01-27 11:13:08 +0300

    Initial commit

---
+++ src/main/java/com/example/anistestbot/AnisTestBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.anistestbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnisTestBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnisTestBotApplication.class, args);
+    }
+
+}
